5ffae2b466b56f20a190fd0bbbdd0a883d2ee99b
Prashanth Batchu <prashanth@batchu.net>
2022-10-14 23:20:32 -0400

    Added a dropdown for versioned requirements. Fixed issues with double conversion to int (eg: 2.)

--- lib/screens/requirement_edit.dart
+++ lib/screens/requirement_edit.dart
@@ -330,40 +330,25 @@ class _VersionRequirementState extends State<VersionRequirement> {
           ),
           //styling
 
-          Container(
-              padding: EdgeInsets.all(15),
-              child: Center(
-                  child: TextField(
-                controller: dateInput,
-                //editing controller of this TextField
-                decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today), //icon of text field
-                    labelText: "Enter the last completed date" //label text of field
-                    ),
-                readOnly: true,
-                //set it true, so that user will not able to edit text
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2022),
-                      //DateTime.now() - not to allow to choose before today.
-                      lastDate: DateTime.now());
-
-                  if (pickedDate != null) {
-                    print(
-                        pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                    String formattedDate =
-                        DateFormat('MM/dd/yyyy').format(pickedDate);
-                    print(
-                        formattedDate); //formatted date output using intl package =>  2021-03-16
-                    setState(() {
-                      dateInput.text =
-                          formattedDate; //set output date to TextField value.
-                    });
-                  } else {}
-                },
-              ))),
+          DropdownButton<int>(
+            value: map['completedVersion'].toInt(), //selected
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Theme.of(context).accentColor),
+            underline: Container(
+              height: 2,
+              color: Colors.blueAccent,
+            ),
+            onChanged: (int? newValue) {},
+            items: [for (var i = map['completedVersion'].toInt() as int; i <= map['requiredVersion']; i++) i]
+                .map<DropdownMenuItem<int>>((int value) {
+              return DropdownMenuItem<int>(
+                value: value,
+                child: Text(value.toString()),
+              );
+            }).toList(),
+          ),
 
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
