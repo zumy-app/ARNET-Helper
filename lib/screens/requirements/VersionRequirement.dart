441829aab8d041bce0be0a5bc0c75c39035f116e
Prashanth Batchu <prashanth@batchu.net>
2022-10-24 22:52:28 -0400

    Fixed width

--- lib/screens/requirements/VersionRequirement.dart
+++ lib/screens/requirements/VersionRequirement.dart
@@ -80,44 +80,41 @@ class _VersionRequirementState extends State<VersionRequirement> {
             ],
           ),
           Container(
-              padding: EdgeInsets.all(15),
-            width: 100,
+
+            width: 250,
               child: Center(
-                  child:  SizedBox(
-                    width: 100.0,
-                    child: TextField(
+                  child:  TextField(
 
-                      controller: dateInput,
-                      //editing controller of this TextField
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.calendar_today), //icon of text field
-                          labelText:
-                          "Enter the new completion date" //label text of field
-                      ),
-                      readOnly: true,
-                      //set it true, so that user will not able to edit text
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2022),
-                            //DateTime.now() - not to allow to choose before today.
-                            lastDate: DateTime.now());
-
-                        if (pickedDate != null) {
-                          print(
-                              pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                          String formattedDate =
-                          DateFormat('MM/dd/yyyy').format(pickedDate);
-                          print(
-                              formattedDate); //formatted date output using intl package =>  2021-03-16
-                          setState(() {
-                            dateInput.text =
-                                formattedDate; //set output date to TextField value.
-                          });
-                        } else {}
-                      },
+                    controller: dateInput,
+                    //editing controller of this TextField
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.calendar_today), //icon of text field
+                        labelText:
+                        "Enter the new completion date" //label text of field
                     ),
+                    readOnly: true,
+                    //set it true, so that user will not able to edit text
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2022),
+                          //DateTime.now() - not to allow to choose before today.
+                          lastDate: DateTime.now());
+
+                      if (pickedDate != null) {
+                        print(
+                            pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                        String formattedDate =
+                        DateFormat('MM/dd/yyyy').format(pickedDate);
+                        print(
+                            formattedDate); //formatted date output using intl package =>  2021-03-16
+                        setState(() {
+                          dateInput.text =
+                              formattedDate; //set output date to TextField value.
+                        });
+                      } else {}
+                    },
                   ),
                   )),
 
